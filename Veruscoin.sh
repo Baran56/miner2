a97766bfb8e865894602bee595482e65105a3ea4
PutraFebrian <88669594+PutraFebrian@users.noreply.github.com>
2021-08-09 21:55:29 +0700

    Create Veruscoin.sh

---
+++ Veruscoin.sh
@@ -0,0 +1,5 @@
+#Veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RYAEAWTXtm9FU7ku8CzcCuM4E4jV3zhsbQ.Putra -p x --cpu 2
